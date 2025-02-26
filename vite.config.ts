7fa728b3f40f7a1caf2760d4f286f755541dffc5
Alexander Zaytsev <sanyageologist@mail.ru>
2025-02-26 20:44:00 +0300

    project start

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    open: true,
+    port: 5001,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [react()],
+})
